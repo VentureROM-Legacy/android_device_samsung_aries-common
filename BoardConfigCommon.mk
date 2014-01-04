b3f817b5a144c7cf3c057e8094f30e9026895bac
SferaDev <SferaDev@users.noreply.github.com>
2014-01-04 11:22:24 +0100

    Build as CWM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -97,6 +97,7 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 
 # Recovery
+BOARD_RECOVERY_CWM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 # Disable for TWRP
